70d991afd48ad626b07b8a98eb172a322debaf7a
MaksimKazak <kazakmaksims@gmail.com>
2018-03-11 16:47:40 +0300

    Do 21 task in 1 lab

---
+++ 2018/09_kmaxim/lab01/lab01_z21.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+arr=($@)
+i=0
+while [ $i -lt $# ]
+do
+  if [ "${arr[$i]}" == "-task" ];
+  then
+    echo "Задание"
+    echo "21. Передать в скрипт два строковых параметра и сравнить их, используя оператор if, если параметры пустые, то вывести предупреждение"
+    echo ""
+    echo ""
+    break
+  fi
+  ((i++))
+done
+
+if [ $# -lt "2" ]
+then
+  echo "Not enough arguments"
+  exit
+fi
+
+if [[ "$1" < "$2" ]]
+then
+  echo "$1 less than $2"
+fi
