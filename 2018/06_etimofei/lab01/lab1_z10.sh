dc5a25dc25dfdad323e130f879b58741caee7ba2
timaxin <timaxin@gmail.com>
2018-03-15 22:50:10 +0300

    Do 10 task in 1 lab

---
+++ 2018/06_etimofei/lab01/lab1_z10.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -n "$1" ] && [ "$1" == "-task" ];
+then
+	echo "Задание"
+	echo "10. Проверить положительное или отрицательное число."
+	echo 
+	echo
+fi
+	declare num
+	read num
+if test $num -ge 0
+then
+	echo "Number is positive"
+else
+	echo "Number is negative"
+fi
+	
