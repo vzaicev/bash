b35810897ac1acf4b9ffd7691fb66bff7144b60e
timaxin <timaxin@gmail.com>
2018-03-16 12:03:38 +0300

    Do 11 task in 1 lab

---
+++ 2018/06_etimofei/lab01/lab1_z11.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -n "$1" ] && [ "$1" == "-task" ];
+then
+	echo "Задание"
+	echo "11. Вывести последний аргумент предыдущей команды."
+	echo 
+	echo
+fi
+echo The last arguement was ${!#}
