79a219b219a9de437e097904a3000a2f4bb58eb2
stasOR4ik <kov-stas99@mail.ru>
2018-05-18 21:35:42 +0300

    Lab 2 - Kovalevsky

---
+++ 2018/10_kstanislav/lab02/Song.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MusicSite
+{
+    public partial class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public int SingerId { get; set; }
+        public int AlbumId { get; set; }
+
+        public Album Album { get; set; }
+        public Singer Singer { get; set; }
+
+        public Song() { }
+
+        public Song(int id, string name, string text, int singerId, int albumId)
+        {
+            Id = id;
+            Name = name;
+            Text = text;
+            SingerId = singerId;
+            AlbumId = albumId;
+        }
+    }
+}
