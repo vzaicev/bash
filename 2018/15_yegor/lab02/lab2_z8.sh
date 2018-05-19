5e5e25ef1017e7923b9cb111eac96a6cc4c2d330
Ersh1237 <36787400+Ersh1237@users.noreply.github.com>
2018-05-19 08:53:17 +0300

    lab 2 all

---
+++ 2018/15_yegor/lab02/lab2_z8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+if [ -n $1 ] && [ "$1" == "-task" ];then 
+echo "Задание" 
+echo "8. Напишите скрипт, который скачивает заглавную страницу https://www.onliner.by/ или другого сайта сохраняет ее в файл. После по этому файлу формирует список уникальных русских слов (использовать wget)." 
+echo 
+echo 
+fi 
+wget http://www.bbc.com/ &>/dev/null 
+ourpage=$(cat index.html) 
+echo "$ourpage" | sed -e 's/\s+/ /g' -e 's/\(.*\)/\L\1/g' -e 's/[^A-Za-z ]//g' -e :a -e 's/<[^>]*>//g;/</N;//ba' | tr ' ' '\n' | sort | uniq 
+exit 0
