56380802a09f2ed61511994161a2c8c7090a9f74
MaksimKazak <kazakmaksims@gmail.com>
2018-05-23 00:19:00 +0300

    Add third lab

---
+++ 2018/09_kmaxim/lab03/lab03_z36.sh
@@ -0,0 +1,24 @@
+﻿#!/bin/bash
+
+arr=($@)
+i=0
+while [ $i -lt $# ]
+do
+  if [ "${arr[$i]}" == "-task" ];
+  then
+    echo "Задание"
+    echo "36. Добавить запятые к числу с десятичной частью и знаком минуса"
+    echo ""
+    echo ""
+    break
+  fi
+  ((i++))
+done
+
+if [ $# -ne 1 ]; then
+  echo "Need at least one argument."
+  exit
+fi
+
+cat $1 | sed ':a;s/(^|[^0-9.])([0-9]+)([0-9]{3})/\1\2,\3/g;ta'
+
---
+++ 2018/09_kmaxim/lab03/lab03_z44.sh
@@ -0,0 +1,24 @@
+﻿#!/bin/bash
+
+arr=($@)
+i=0
+while [ $i -lt $# ]
+do
+  if [ "${arr[$i]}" == "-task" ];
+  then
+    echo "Задание"
+    echo "44. Печатать предпоследнюю строку в файле"
+    echo ""
+    echo ""
+    break
+  fi
+  ((i++))
+done
+
+if [ $# -ne 1 ]; then
+  echo "Need at least one argument."
+  exit
+fi
+
+cat $1 | sed -e '$!{h;d;}' -e x
+
