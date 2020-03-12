9b945b3c2330e6873b1fd058b87c44ff61ec5b3e
Deniel <Drowpall@gmail.com>
2020-03-12 20:26:48 +0300

    Update z6.sh

--- 2020/09_Ochkanov/Lab01/z6.sh
+++ 2020/09_Ochkanov/Lab01/z6.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -n "$1" ] && [ "$1"  == "-task" ]; then
-echo "Задание"
-echo "4. Вывести имя скрипта, который был запущен."
+echo "Task"
+echo "4. Output a launched script name."
 echo 
 echo
 fi
-echo "Название файла: ${0##*/}"        
+echo "File name: ${0##*/}"        
 exit 0
