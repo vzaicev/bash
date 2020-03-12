ae49ea4a49b5c8520bbdf4f3e049f8a7d81bd22c
Deniel <Drowpall@gmail.com>
2020-03-12 20:27:52 +0300

    Update z28.sh

--- 2020/09_Ochkanov/Lab01/z28.sh
+++ 2020/09_Ochkanov/Lab01/z28.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -n "$1" ] && [ "$1" == "-task" ]; then
 echo "Task"
-echo "Create a script, which processes the file list, given in a file, and removes those files according to a given list (Use for cycle for this task)"
+echo "28. Create a script, which processes the file list, given in a file, and removes those files according to a given list (Use for cycle for this task)"
 echo
 echo
 fi
