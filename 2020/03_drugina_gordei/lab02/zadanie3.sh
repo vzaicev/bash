ab9d1b2f512d6265a7d02745e76da0ae735f4f85
Nerbet <60988912+Nerbet@users.noreply.github.com>
2020-05-24 20:49:15 +0300

    Add files via upload

---
+++ 2020/03_drugina_gordei/lab02/zadanie3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+if [ -n "$1" ] && [ "$1" == "-task" ]; then
+echo "Задание"
+echo "3. Создайте функцию, в которую передается текст, состоящий из более 20 слов."
+echo "Текст передается из файла. Функция обрабатывает текст выводит каждое второе слово."
+echo
+echo
+fi
+function myf
+{
+touch file
+buf1="Один два три четыре пять шесть семь восемь девять десять одиннадцать двенадцать тринадцать четырнадцать пятнадцать шестнадцать семнадцать восемнадцать девятнадцать двадцать двадцать один двадцать два двадцать три двадцать четыре двадцать пять двадцать шесть."
+echo $buf1
+buf2=`echo -n $buf1 | sed 's/ /\n/g'`
+echo -e "$buf2" >>file
+echo
+echo
+echo $buf2
+echo
+echo
+readarray ARRAY < file
+index=0
+while read line; do
+array[$index]="$line"
+index=$(($index+1))
+done < file
+for ((a=1; a < ${#array[@]}; a+=2))
+do
+echo ${array[$a]}
+done
+rm file
+}
+myf
+
+
