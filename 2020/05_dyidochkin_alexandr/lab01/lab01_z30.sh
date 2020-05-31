1f1e8a51aa53c37c794afea414899dd38533739c
Sasha Didochkin <didochkin.sasha@gmail.com>
2020-05-31 23:06:37 +0300

    add lab

---
+++ 2020/05_dyidochkin_alexandr/lab01/lab01_z30.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ -n "$1" ] && [ "$1"  == "-task" ];then
+echo "Задание"
+echo "30. Сформировать список параметров, используя команду read и цикл until,"
+echo "затем вывести все введенные параметры в обратном порядке."
+echo
+echo
+fi
+declare -i numberOfElements
+echo "Введите количество параметров :"
+read numberOfElements
+echo "Введите параметр(ы) :"
+until [ $numberOfElements = 0 ]
+do
+read temp
+array+=([$numberOfElements - 1]=$temp)
+let "numberOfElements = $numberOfElements - 1"
+done
+echo
+echo ${array[*]}
+exit 0
