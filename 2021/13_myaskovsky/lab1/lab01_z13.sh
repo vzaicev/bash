56ab9e10bd19c45a0526c115a4f324d5d51e6700
myaskovsky <yaroslav.myaskovsky@gmail.com>
2021-03-20 15:44:52 +0300

    finished labs 13, 17

--- 2021/13_myaskovsky/lab1/lab01_z13.sh
+++ 2021/13_myaskovsky/lab1/lab01_z13.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-if [ -n "$1" ] && [ "$1" == "-task" ]; then 
-echo "Задание 13"
-echo "Сравнить две числовые переменные и если первая больше, то вывести сообщение об этом, используя команду test"
+arr=($@)
+i=0
+while [ $i -lt $# ]
+do
+  if [ "${arr[$i]}" == "-task" ];
+  then
+    echo "Задание"
+    echo "13. Сравнить две числовые переменные и если первая больше,"
+    echo "то вывести сообщение об этом, используя команду test"
+    echo ""
+    break
+  fi
+  ((i++))
+done
+
+echo "Enter first number: "
+read num1
+echo "Enter second number: "
+read num2
+
+re='^[0-9]+$'
+if ! [[ $num1 =~ $re ]] || ! [[ $num2 =~ $re ]];
+then
+  echo "error: Not a number" >&2;
+  exit 1
 fi
 
-if [ "$1" == "test" ]; then
-	firstNum=$2
-	secondNum=$3
-
-	if (( firstNum>secondNum ));
-		then echo "$2 greater then $3";
-	fi
+if test $num1 -gt $num2
+then
+  echo "First number is greater than second"
 fi
-
-exit 0
