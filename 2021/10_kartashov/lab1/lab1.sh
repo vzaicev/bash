b1f639a1c5388d5753efd07594bf5243d31cf028
Angibur <ivan_kartashov03@mail.ru>
2021-04-10 12:08:46 +0900

    Script for first lab was added

---
+++ 2021/10_kartashov/lab1/lab1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$1" = "-task" ] && [ -n "$1" ]; then echo "Задание:"
+echo "1. Создать скрипт выдающий сообщение"
+echo " "
+echo " "
+fi; echo "Первое сообщение"
+echo "Если передать параметр -task то оно будет не первым"
+echo "Fsadpklfp"
+echo "The end"
