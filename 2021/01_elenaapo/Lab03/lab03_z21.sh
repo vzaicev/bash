89d67d582b156da59487b633703ea77689e2dbce
Elena-Apo <elena.apochkina@mail.ru>
2021-05-16 17:55:03 +0300

    All 3th lab

---
+++ 2021/01_elenaapo/Lab03/lab03_z21.sh
@@ -0,0 +1,24 @@
+﻿#!/bin/bash
+
+arr=($@)
+i=0
+while [ $i -lt $# ]
+do
+  if [ "${arr[$i]}" == "-task" ];
+  then
+    echo "Задание"
+    echo "20. Поиск и замена "Hi\bro" на "Yo\bro" (замена только первых 4рех совпадения в строке)"
+    echo ""
+    echo ""
+    break
+  fi
+  ((i++))
+done
+
+if [ $# -ne 4 ]; then 
+  echo "Need at least one argument."
+  exit
+fi
+
+cat $1 | sed 's/Hi\\bro/Yo\\bro/' file.txt
+
