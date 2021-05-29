72eff03df6a0d37ed8d2bc0f999805f0f83874bf
RuslanKulesh <nkuleshruslan@gmail.com>
2021-05-29 20:07:24 +0300

    finished lab3 exact 1, 70, 30

---
+++ 2021/10_KuleshRuslan/lab3/lab03_z20.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+arr=($@)
+i=0
+while [ $i -lt $# ]
+do
+  if [ "${arr[$i]}" == "-task" ];
+  then
+    echo "Задание"
+    echo "20. Поиск и замена "Hi\bro" на "Yo\bro" (замена только первого совпадения в строке)"
+    echo ""
+    echo ""
+    break
+  fi
+  ((i++))
+done
+
+if [ $# -ne 1 ]; then
+  echo "Need at least one argument."
+  exit
+fi
+
+cat $1 | sed 's/Hi\\bro/Yo\\bro/'
+
