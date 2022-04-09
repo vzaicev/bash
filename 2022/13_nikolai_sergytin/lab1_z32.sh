847f14f98b91213304652b8eca335a5fa085b899
Nickolay03 <100015199+Nickolay03@users.noreply.github.com>
2022-04-09 17:57:05 +0300

    Create lab1_z32.sh

---
+++ 2022/13_nikolai_sergytin/lab1_z32.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+if [ -n "$1" ] && [ "$1"  == "-task" ];
+then
+	echo "Задание"
+	echo "32. Создать функцию распознавания переданного параметра"
+	echo "и использовать ее внутри функции,"
+	echo "обрабатывающей параметры внутри скрипта"
+	echo
+  echo
+fi
+
+function isParams {
+	if [ -n "$1" ] 
+	then
+		return  0
+	else
+		return 1
+	fi
+}
+
+function isNumber {
+	if [ $1 -eq $1 2> /dev/null ]; 
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function print {
+	for var in "$@"
+	do
+		if isNumber $var; then
+			echo $var
+		fi
+	done
+}
+
+function main {
+	if isParamsPassed $1; then 
+		printNumbers "$@"
+	else 
+		echo no params
+	fi
+} 
+
+main "$@"
