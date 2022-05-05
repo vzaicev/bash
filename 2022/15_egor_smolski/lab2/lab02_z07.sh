592ec7c574ca125c6819c09f6112cdf8aefac1a5
Smolskiy-Egor <egorka.smolskiy@mail.ru>
2022-05-05 14:30:27 +0300

    Scripts for the second lab added

---
+++ 2022/15_egor_smolski/lab2/lab02_z07.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -n "$1" ] && [ "$1" == "-task" ]; then
+echo "Задание"
+echo "7. Напишите скрипт, который скачивает заглавную страницу https://www.onliner.by/ или другого сайта и сохраняет её в файл. После по этому файлу формирует список уникальных русских слов (использовать wget)."
+echo " "
+echo " "
+fi
+wget https://www.onliner.by/ &>/dev/null
+ourpage=$(cat index.html)
+echo "$ourpage" | sed -e 's/\s+/ /g' -e 's/\(.*\)/\L\1/g' -e 's/[^А-яуЕёЁ ]//g' | tr ' ' '\n' | sort | uniq
+exit 0
