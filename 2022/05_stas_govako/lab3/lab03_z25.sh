5c27284e7a0e0ff3507d267078deb4b49321568f
stas0govako <stas.govako.02@mail.ru>
2022-05-15 13:09:40 +0300

    Scripts for the lab 3 were added

---
+++ 2022/05_stas_govako/lab3/lab03_z25.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -n "$1" ] && [ "$1" == "-task" ];
+then
+	echo "Задание"
+	echo "25. Замена \"Hi\bro\" на \"Yo\bro\" только для строк содержащих \"Black\""
+	echo 
+	echo
+fi
+	sed '/Black/s/Hi\\bro/Yo\\bro/g' file.txt
