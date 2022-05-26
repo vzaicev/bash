11a1af826b1ae4ad9fb24e288faa370d2cecdd4d
DaniiilDub <daniil.dubaleko@bk.ru>
2022-05-26 20:20:57 +0300

    Scripts for the second lab were added!

---
+++ 2022/06_daniil_dybaleko/lab2/mycode.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>;
+#include "House.h";
+using namespace std;
+void main()
+{
+	House* spis;
+	int n;
+	cout << "Input a number of houses: "; cin >> n;
+	spis = new House[n];
+	for (int i = 0; i < n; i++) {
+		cout << "==============================" << endl;
+		spis[i].show();
+		
+	}
+	spis1(spis, n);
+	spis2(spis, n);
+	spis3(spis, n);
+	delete[] spis;
+	cout << "press any key!";
+	system("pause");
+	return;
+House* spis;
+	int n;
+	cout << "Input a number of houses: "; cin >> n;
+	spis = new House[n];
+	for (int i = 0; i < n; i++) {
+		cout << "==============================" << endl;
+		spis[i].show();
+		
+	}
+	spis1(spis, n);
+	spis2(spis, n);
+	spis3(spis, n);
+	delete[] spis;
+	cout << "press any key!";
+	system("pause");
+	return;
+House* spis;
+	int n;
+	cout << "Input a number of houses: "; cin >> n;
+	spis = new House[n];
+	for (int i = 0; i < n; i++) {
+		cout << "==============================" << endl;
+		spis[i].show();
+		
+	}
+	spis1(spis, n);
+	spis2(spis, n);
+	spis3(spis, n);
+	delete[] spis;
+	cout << "press any key!";
+	system("pause");
+	return;
+}
